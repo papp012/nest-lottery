3996440034efc9204268f417cb373ba72f084e18
papp012 <papp.csenge0@gmail.com>
2023-03-06 09:13:26 +0100

    feat: getting winning numbers from post request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,15 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  readWinningNumbers(): string {
+    return this.appService.readWinningNumbers();
+  }
+
+  @Post()
+  writeWinningNumbers(
+    @Body('numbers') winningNumbers: string,
+  ) {
+    console.log(winningNumbers);
+    this.appService.writeWinningNumbers();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  readWinningNumbers(): string {
+    return '3 4 5 6 7';
+  }
+
+  writeWinningNumbers() {
+    // posting winning lottery numbers of this week
+  };
+
+  storeWinningNumbers() {
+    // store winning lottery numbers in persistent storage
   }
 }
