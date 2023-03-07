17076a0f41f823dbdc0d2dd330f09bb70eb8d810
papp012 <papp.csenge0@gmail.com>
2023-03-07 07:33:36 +0100

    feat: use redis to cache

--- src/lottery/lotterynumbers.module.ts
+++ src/lottery/lotterynumbers.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { RedisModule } from '@nestjs-modules/ioredis'
 import { DatabaseModule } from '../database/database.module';
 import { LotteryNumbersService } from './lotterynumbers.service';
 import { lotteryNumbersProviders } from './lotterynumbers.providers';
 import { LotteryNumbersController } from './lotterynumbers.controller';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    RedisModule.forRoot({
+        config: {
+          url: 'redis://localhost:6379'
+        },
+      }),],
   controllers: [LotteryNumbersController],
   providers: [
     LotteryNumbersService,
--- src/lottery/lotterynumbers.service.ts
+++ src/lottery/lotterynumbers.service.ts
@@ -2,6 +2,7 @@ import { Model } from 'mongoose';
 import { Injectable, Inject } from '@nestjs/common';
 import { LotteryNumbers } from './interfaces/lotterynumbers.interface';
 import { CreateLotteryNumbersDto } from './dto/create.lotterynumbers.dto';
+import { InjectRedis,Redis } from "@nestjs-modules/ioredis";
 
 
 @Injectable()
@@ -9,18 +10,32 @@ export class LotteryNumbersService {
   constructor(
     @Inject('LOTTERYNUMBERS_MODEL')
         private readonly lotteryNumbersModel: Model<LotteryNumbers>,
+    @InjectRedis() private readonly redis: Redis,
   ) {}
 
   async writeWinningNumbers(createLotteryNumbersDto: CreateLotteryNumbersDto): Promise<LotteryNumbers> {
     const winningNumbers = new this.lotteryNumbersModel({winningNumbers: createLotteryNumbersDto});
-    return winningNumbers.save();
+    return winningNumbers.save();   
   }
 
   async readWinningNumbers() {
-    const winningNumbers = await this.lotteryNumbersModel.find().exec();
-    return winningNumbers.map(numbers => ({
-      "winning numbers": numbers.winningNumbers,
-    }));
+    if (await this.redis.get("winning-numbers") === null ) {
+          console.log("from db")
+          const winningNumbers = await this.lotteryNumbersModel.find().exec();
+
+          console.log(winningNumbers[0].winningNumbers);
+          //this.redis.rpush("winning-numbers",...winningNumbers[0].winningNumbers);
+          this.redis.set("winning-numbers", winningNumbers[0].winningNumbers);
+      
+          return winningNumbers.map(numbers => ({
+                "winning numbers": numbers.winningNumbers,
+              }));
+        
+        } 
+        else {
+          console.log("from redis")
+          return await this.redis.get("winning-numbers")
+        }
   }
 
   async deleteWinningnumbers() {
